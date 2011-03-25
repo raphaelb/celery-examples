99c846e6da2a31e237fbbc362f6910861ff2a5e9
Raphael Burnes <raphael.burnes@gmail.com>
2011-03-25 12:51:20 -0500

    Added an excellent comment explaining aardvarks

--- tasks.py
+++ tasks.py
@@ -3,7 +3,8 @@ from celery.decorators import task
 @task
 def make_pi(num_calcs):
     print "Approximating pi with %s calculations" % num_calcs
-    pi = 0.0
+
+    pi = 0.0 # set pi to 0.0
     for k in xrange(num_calcs):
         pi += 4 * ((-1)**k / ((2.0 * k) + 1))
     return pi
